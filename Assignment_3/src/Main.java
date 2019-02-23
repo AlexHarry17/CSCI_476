94d4879a0e6764495ac60f0ff80fe71abd90396e
Brick7Face <njtranel@gmail.com>
2019-02-23 16:36:11 -0700

    everything works but it takes forever

--- Assignment_3/src/Main.java
+++ Assignment_3/src/Main.java
@@ -7,31 +7,44 @@
 
 //uses crackstation's human password wordlist dictionary - https://crackstation.net/crackstation-wordlist-password-cracking-dictionary.htm
 
+import javax.xml.bind.DatatypeConverter;
 import java.io.*;
 import java.util.ArrayList;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-        BufferedReader input = new BufferedReader(new FileReader("/home/brick/Desktop/CSCI_476/Assignment_3/pw.txt"));   // Reads in encrypted passwords file
-        String current = input.readLine();
+        FileInputStream pwds = new FileInputStream("../pw.txt");   // Reads in encrypted passwords file
+        Scanner s = new Scanner(pwds);
         ArrayList<String> passwords = new ArrayList<String>();
-        while(current != null) {
-            passwords.add(current);         // Loops through the file lines, add current line to an array list of encrypted passwords
+        while (s.hasNextLine()) {
+            passwords.add(s.nextLine());         // Loops through the file lines, add current line to an array list of encrypted passwords
         }
+        s.close();
 
         //uses dictionary attack to encrypt all passwords in dictionary and compare them to the MD5 hashes that exist
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             for(int i = 0; i < passwords.size(); i++) {
-                BufferedReader pwdictionary = new BufferedReader(new FileReader("/home/brick/Desktop/CSCI_476/Assignment_3/dictionary.txt"));
-                String currentPlaintext = pwdictionary.readLine();
-                String currentHash = "Encrypted version of plaintext --placeholder";
-                //encrypt currentHash with md instance, compare that hash to the current password hash
-                if (currentHash != null && currentHash.equals(current)) {
-                    //if they match, then return that password plaintext
+                long startTime = System.currentTimeMillis();
+                FileInputStream pwdictionary = new FileInputStream("../dictionary.txt");
+                Scanner sc = new Scanner(pwdictionary, "UTF-8");
+                while (sc.hasNextLine()) {
+                    String currentPlaintext = sc.nextLine();
+                    md.update(currentPlaintext.getBytes());
+                    byte[] digest = md.digest();
+                    String currentHash = DatatypeConverter.printHexBinary(digest).toLowerCase();
+                    //encrypt currentHash with md instance, compare that hash to the current password hash
+                    if (currentHash.equals(passwords.get(i))) {
+                        //if they match, then print that password plaintext
+                        long stopTime = System.currentTimeMillis();
+                        System.out.printf("The password for hash value %s is %s, it takes the program %d sec to recover this password\n", passwords.get(i), currentPlaintext, ((stopTime - startTime)/1000));
+                        break;
+                    }
                 }
+                sc.close();
             }
         }
         catch (NoSuchAlgorithmException e) {
